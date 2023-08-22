cf21f3bdeb26c298cef13aaddd5ff286461bf4ff
NicolasAmorosi <nicolasamorosi@gmail.com>
2023-08-22 13:12:53 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <Text className=" text-3xl text-cyan-600 font-bold">Pito</Text>
+    </View>
+  );
+}
